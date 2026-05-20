6b1620abc6a8dcb561bee99e4061970bdcd86b22
antyez <antyez.dev@gmail.com>
2026-05-20 21:54:05 +0100

    add global conversion funnel to queries.sql (session based)

---
+++ sql/02_queries/01_conversion_funnel.sql
@@ -0,0 +1,37 @@
+-- Hypothesis: Are users getting stuck somewhere? Does this business capitalize on their product views?
+-- Standard conversion funnel (session based)
+
+SET search_path TO ecommerce_events;
+
+CREATE VIEW v_session_conversion_funnel AS (   
+
+WITH funnel_counts AS (
+	SELECT
+		COUNT(DISTINCT user_session) AS total_views,
+		COUNT(DISTINCT CASE WHEN event_type = 'cart' THEN user_session END) AS total_add_to_cart,
+		COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_session END) total_purchases
+	FROM
+		events
+)
+
+SELECT
+	-- TOTAL VOLUMES --
+	total_views,
+    total_add_to_cart,
+    total_purchases,
+    -- possible view jail
+
+    -- RATES --
+    -- how many views became part of a cart? (Testing the view jail hypothesis) | view -> cart.
+	ROUND((total_add_to_cart::NUMERIC / total_views) * 100, 2) AS view_to_cart_rate, 
+    -- how many carts were bought in percentage? (Checkout efficiency) cart -> purchase.
+    ROUND((total_purchases::NUMERIC / NULLIF(total_add_to_cart, 0)) * 100, 2) AS cart_to_purchase_rate,
+    -- out of the total views, how many  ended up in a purchase? (Global conversion rate) | view -> cart -> purchase.
+	ROUND((total_purchases::NUMERIC / total_views) * 100, 2) AS view_to_purchase_rate
+
+FROM
+	funnel_counts;
+)
+
+-- Conclusion: 'view jail' hypothesis confirmed, out of all the views, only 8% of products were added to a cart and only 5% of the views were a purchase.
+-- Acceptable cart to purchase rate = 59%, acceptable checkout efficiency by increasing views to cart rate might be the best way to increase purchases.
